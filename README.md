d6991f21f27bc64f36650b959a941782688fc2bf
KanekiiKat <alex123viri@gmail.com>
2025-03-27 13:33:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# BrunosBox Kata
+BrunosBox kata
+
+<img src="diagrama_clases_UML_alumnado.png" alt="Uml brunos">
---
+++ src/test/java/edu/estatuas/brunosbox/BrunosboxTest.java
@@ -0,0 +1,10 @@
+package edu.estatuas.brunosbox;
+
+import org.junit.jupiter.api.Test;
+
+public class BrunosboxTest {
+
+    @Test public void testAppExistis() {
+    }
+
+}
